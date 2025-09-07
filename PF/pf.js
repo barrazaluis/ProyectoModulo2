5d5c3cbd4634c93db4cc933b06b71263b0c470c2
barrazaluis <cbarrazaluis@gmail.com>
2025-09-06 23:34:02 -0400

    Archivo Elinado: Reconstruyendo Commits: menu de inicio

--- PF/pf.js
+++ PF/pf.js
@@ -1,27 +1,2 @@
-alert("bienvenido al sistema de encuestas")
+alert("Bienvenido al sistema de encuestas");
 
-function inicio(){
-let salir = false;
-while(!salir){
-    let seleccion = Number(prompt("sistema de tickets\n" +
-      "Seleccione la opción deseada:\n" +
-      "1. crear nueva encuesta\n" +
-      "2. votar en encuesta existente\n" +
-      "3. mostrar resultados de encuentas\n" +
-      "4. Salir"))
-
-if(seleccion === 1){
-    nuevaEncuesta();
-}else if(seleccion === 2){
-    votarEnEncuesta();
-}
-else if(seleccion === 3){
-    mostrarResultado();
-}
-else if (seleccion === 4) {
-    alert("Gracias por usar el sistema de encuestas");
-    salir = true;
-}
-}
-}
-inicio()
