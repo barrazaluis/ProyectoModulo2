599033ffa82cfb2b14c300601a9387fc5f70c8f0
barrazaluis <cbarrazaluis@gmail.com>
2025-09-06 23:36:38 -0400

    Archivo Elinado: Reconstruyendo Commits: funcion generar encuesta

--- PF/pf.js
+++ PF/pf.js
@@ -1,2 +1,34 @@
 alert("Bienvenido al sistema de encuestas");
 
+let encuestas = []; 
+let resultados = [];
+
+function inicio() {
+    let salir = false;
+
+    while (!salir) {
+        let seleccion = Number(prompt(
+            "Sistema de tickets\n" +
+            "Seleccione la opción deseada:\n" +
+            "1. Crear nueva encuesta\n" +
+            "2. Votar en encuesta existente\n" +
+            "3. Mostrar resultados de encuestas\n" +
+            "4. Salir"
+        ));
+
+        if (seleccion === 1) {
+            generarEncuesta();
+        } else if (seleccion === 2) {
+            votarEnEncuesta();
+        } else if (seleccion === 3) {
+            mostrarResultado();
+        } else if (seleccion === 4) {
+            alert("Gracias por usar el sistema de encuestas");
+            salir = true;
+        } else {
+            alert("Opción inválida. Intente nuevamente.");
+        }
+    }
+}
+
+inicio();
